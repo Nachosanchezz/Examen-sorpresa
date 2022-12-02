15e88258fe2dc1d9d6f9f1587269ca83a6964662
Nacho <isanccal@myuax.com>
2022-12-02 18:10:50 +0100

    Cambio 2 Sudoku

--- SudokuSolve.py
+++ SudokuSolve.py
@@ -2,4 +2,10 @@ def sudoku(board):
     (x, y) = find_empty_cell(board)
     if (x, y) == (-1, -1):
         return True
-        
+    for i in {1,2,3,4,5,6,7,8,9}:
+        if valid(x,y,i,board):
+            board[x][y] = i
+            if sudoku(board):
+                return board
+            board[x][y] = 0
+
